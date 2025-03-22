6861c947d8f54659c9e852a2027f6b3b4dd55050
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-22 22:28:35 +0700

    done JPA for Review(Data modeling) 22/3

--- database/src/main/java/ut/edu/database/models/Review.java
+++ database/src/main/java/ut/edu/database/models/Review.java
@@ -1,6 +1,90 @@
 package ut.edu.database.models;
 import jakarta.persistence.*;
+import java.time.LocalDate;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+import java.util.Objects;
 
+@Entity
+@Table(name = "Review")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "userID")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "propertyID")
+    private Property property;
+
+
+    @Min(1)
+    @Max(5)
+    private int rating; //1-5
+    private String comment;
+    private LocalDate reviewDate;
+
+    //Constructors
+    public Review() {
+
+    }
+    public Review(User user, Property property, int rating, String comment, LocalDate reviewDate) {
+        this.user = user;
+        this.property = property;
+        setRating(rating); // Sử dụng setter
+        this.comment = comment;
+        this.reviewDate = reviewDate;
+    }
+
+    //Getters
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public LocalDate getReviewDate() {
+        return reviewDate;
+    }
+
+    //Setters
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public void setRating(int rating) {
+        if(rating<1 ||rating>5){
+            throw new IllegalArgumentException("Rating must be between 1 and 5");
+        }
+        this.rating = rating;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setReviewDate(LocalDate reviewDate) {
+        this.reviewDate = reviewDate;
+    }
 }

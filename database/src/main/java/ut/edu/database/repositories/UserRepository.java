6874c008d36afb1b7055afb5a4c0cedfb1ad7640
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-23 01:38:33 +0700

    update repo & create db/migration 23/3

--- database/src/main/java/ut/edu/database/repositories/UserRepository.java
+++ database/src/main/java/ut/edu/database/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package ut.edu.database.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ut.edu.database.models.User;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }

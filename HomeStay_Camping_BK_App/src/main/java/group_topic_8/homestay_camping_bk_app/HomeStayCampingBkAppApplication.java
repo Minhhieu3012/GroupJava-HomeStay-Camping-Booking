0ebb143472ed168eeaf17d73a36d889f34b0d5fe
“Dapaull” <ngbao840@gmail.com>
2025-03-23 10:00:45 +0700

    create new project

---
+++ HomeStay_Camping_BK_App/src/main/java/group_topic_8/homestay_camping_bk_app/HomeStayCampingBkAppApplication.java
@@ -0,0 +1,13 @@
+package group_topic_8.homestay_camping_bk_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeStayCampingBkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeStayCampingBkAppApplication.class, args);
+	}
+
+}
---
+++ HomeStay_Camping_BK_App/src/test/java/group_topic_8/homestay_camping_bk_app/HomeStayCampingBkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package group_topic_8.homestay_camping_bk_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeStayCampingBkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

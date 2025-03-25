293395d0a79febd8eae5322d5419eac186db06b1
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-25 14:21:01 +0700

    update BookingService 25/3

--- database/src/main/java/ut/edu/database/services/BookingService.java
+++ database/src/main/java/ut/edu/database/services/BookingService.java
@@ -4,8 +4,19 @@ import ut.edu.database.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookingService {
     @Autowired
     private BookingRepository bookingRepository;
+
+    //
+    public List<Booking> getBookingByUserId(Long userId) {
+        return bookingRepository.findByUserId(userId);
+    }
+    //
+    public List<Booking> getBookingByPropertyId(Long propertyId) {
+        return bookingRepository.findByPropertyId(propertyId);
+    }
 }

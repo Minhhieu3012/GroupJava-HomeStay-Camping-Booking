c9b8639a8b786e83f1df188312213a46df437e76
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-25 13:17:54 +0700

    update Services(property, booking) 25/3

--- database/src/main/java/ut/edu/database/services/BookingService.java
+++ database/src/main/java/ut/edu/database/services/BookingService.java
@@ -1,4 +1,11 @@
 package ut.edu.database.services;
+import ut.edu.database.models.Booking;
+import ut.edu.database.repositories.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookingService {
+    @Autowired
+    private BookingRepository bookingRepository;
 }

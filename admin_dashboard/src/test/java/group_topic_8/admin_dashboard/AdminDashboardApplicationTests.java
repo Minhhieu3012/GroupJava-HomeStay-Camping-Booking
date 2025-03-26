271fa43d9c578a0f0fd2f63e4ec82a5c00bebd13
“Dapaull” <ngbao840@gmail.com>
2025-03-26 23:01:14 +0700

    first commit

---
+++ admin_dashboard/src/test/java/group_topic_8/admin_dashboard/AdminDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package group_topic_8.admin_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ffc8d93e2792a36905ed1b204b093e21eb256712
dtien18 <tiendt4180@ut.edu.vn>
2025-03-27 22:50:07 +0700

    test

---
+++ chotot-travel-backend/src/test/java/com/chotot/travel/ChototTravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chotot.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChototTravelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

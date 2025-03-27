a2d4acb51ad7f1900e5215c2a84915fe88183490
dtien18 <tiendt4180@ut.edu.vn>
2025-03-27 23:22:40 +0700

    test model

---
+++ chotot-travel-backend/src/main/java/com/chotot/travel/model/User.java
@@ -0,0 +1,66 @@
+package com.chotot.travel.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<com.chotot.travel.model.Role> roles = new HashSet<>();
+
+    // Getters và Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<com.chotot.travel.model.Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<com.chotot.travel.model.Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

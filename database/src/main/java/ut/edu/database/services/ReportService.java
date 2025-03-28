9ce3509981d18365355c80479f4b2357717278ae
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-28 11:42:16 +0700

    28/3

--- database/src/main/java/ut/edu/database/services/ReportService.java
+++ database/src/main/java/ut/edu/database/services/ReportService.java
@@ -1,11 +1,26 @@
 package ut.edu.database.services;
 import ut.edu.database.models.Report;
-import ut.edu.database.repositories.ReviewRepository;
+import ut.edu.database.repositories.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReportService {
     @Autowired
-    private ReviewRepository reviewRepository;
+    private ReportRepository reportRepository;
+
+    //
+    public List<Report> getReportsByPropertyId(Long propertyId) {
+        return reportRepository.findByPropertyId(propertyId);
+    }
+    //
+    public List<Report> getReportsByPeriod(String period) {
+        return reportRepository.findByPeriod(period);
+    }
+    //
+    public Report createReport(Report report) {
+        return reportRepository.save(report);
+    }
 }

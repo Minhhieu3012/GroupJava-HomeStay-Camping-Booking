9106cd847f4dc625da10a45f74f0d08fe440cb3f
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-29 00:37:53 +0700

    update UserController 29/3

--- database/src/main/java/ut/edu/database/controllers/UserController.java
+++ database/src/main/java/ut/edu/database/controllers/UserController.java
@@ -1,4 +1,26 @@
 package ut.edu.database.controllers;
+import ut.edu.database.models.User;
+import ut.edu.database.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ut.edu.database.services.UserService;
+import java.util.List;
+import java.util.Optional;
 
+@RestController
+//REST API
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    //lay tat ca user
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    //lay user theo id
+//    @GetMapping("/{id}")
+
+
 }

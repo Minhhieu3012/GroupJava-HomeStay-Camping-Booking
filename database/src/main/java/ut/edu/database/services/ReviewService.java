b045c8184700e4f9db9b6824c82848d270056e1b
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-28 21:47:06 +0700

    done ReviewService 28/3

--- database/src/main/java/ut/edu/database/services/ReviewService.java
+++ database/src/main/java/ut/edu/database/services/ReviewService.java
@@ -4,8 +4,27 @@ import ut.edu.database.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
+
+    //
+    List<Review> getReviewsByPropertyId(Long propertyId) {
+        return reviewRepository.findByPropertyId(propertyId);
+    }
+    //
+    List<Review> getReviewsByUserId(Long userId) {
+        return reviewRepository.findByUserId(userId);
+    }
+    //
+    List<Review> getReviewsByRating(int rating) {
+        return reviewRepository.findByRating(rating);
+    }
+    //
+    public Review createReview(Review review) {
+        return reviewRepository.save(review);
+    }
 }

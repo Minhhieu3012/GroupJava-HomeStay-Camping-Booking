015975e23a4fb6d46bfd85fe1bc30c64989fb204
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-28 11:31:09 +0700

    done PropertyService 28/3

--- database/src/main/java/ut/edu/database/services/PropertyService.java
+++ database/src/main/java/ut/edu/database/services/PropertyService.java
@@ -1,11 +1,30 @@
 package ut.edu.database.services;
 import ut.edu.database.models.Property;
-import ut.edu.database.repositories.ReviewRepository;
+import ut.edu.database.repositories.PropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PropertyService {
     @Autowired
-    private ReviewRepository reviewRepository;
+    private PropertyRepository propertyRepository;
+
+    //
+    public List<Property> getPropertiesByOwnerId(Long ownerId) {
+        return propertyRepository.findByOwnerId(ownerId);
+    }
+    //
+    public List<Property> getPropertiesByLocation(String location) {
+        return propertyRepository.findByLocationContainingIgnoreCase(location);
+    }
+    //
+    public List<Property> getPropertiesByStatus(String status) {
+        return propertyRepository.findByStatus(status);
+    }
+    //
+    public Property createProperty(Property property) {
+        return propertyRepository.save(property);
+    }
 }

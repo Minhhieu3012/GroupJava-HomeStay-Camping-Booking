35ff3e1ea9346d6e112ad5913a048242b676f838
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-29 23:47:28 +0700

    improve service booking 29/3

--- database/src/main/java/ut/edu/database/services/BookingService.java
+++ database/src/main/java/ut/edu/database/services/BookingService.java
@@ -11,19 +11,24 @@ public class BookingService {
     @Autowired
     private BookingRepository bookingRepository;
 
-    //
+    //lay ds booking theo user id
     public List<Booking> getBookingByUserId(Long userId) {
         return bookingRepository.findByUserId(userId);
     }
-    //
+    //lay ds booking theo property id
     public List<Booking> getBookingByPropertyId(Long propertyId) {
         return bookingRepository.findByPropertyId(propertyId);
     }
-    //
+    //lay ds booking theo status
     public List<Booking> getBookingByStatus(String status) {
-        return bookingRepository.findByStatus(status);
+        try{
+            Booking.BookingStatus bookingStatus = Booking.BookingStatus.valueOf(status.toUpperCase()); //chuyen chuoi nhap vaoo thanh chu in hoa, tranh loi nguoi dung nhap chu thuong
+            return bookingRepository.findByStatus(bookingStatus.toString()); //chuyen enum thanh chuoi
+        }catch(IllegalArgumentException e){ //neu gia tri ko loi se xay ra loi
+            return List.of(); //tra ve ds rong
+        }
     }
-    //
+    //tao booking moi
     public Booking createBooking(Booking booking) {
         return bookingRepository.save(booking);
     }

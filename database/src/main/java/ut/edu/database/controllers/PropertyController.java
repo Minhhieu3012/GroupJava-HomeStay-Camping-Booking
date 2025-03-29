5a299667b7fb8e0c47f2ef49a871a121e58a843e
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-03-29 14:54:10 +0700

    done PropertyController 29/3

--- database/src/main/java/ut/edu/database/controllers/PropertyController.java
+++ database/src/main/java/ut/edu/database/controllers/PropertyController.java
@@ -1,4 +1,40 @@
 package ut.edu.database.controllers;
+import ut.edu.database.models.Property;
+import ut.edu.database.repositories.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
 
+@RestController
+@RequestMapping("/properties")
 public class PropertyController {
+    @Autowired
+    private PropertyRepository propertyRepository;
+
+    //lay tat ca bat dong san
+    @GetMapping
+    public List<Property> getAllProperties() {
+        return propertyRepository.findAll();
+    }
+    //lay bat dong san theo id
+    @GetMapping("/{id}")
+    public Optional<Property> getPropertyById(@PathVariable Long id) {
+        return propertyRepository.findById(id);
+    }
+    //tao bat dong san moi
+    @PostMapping
+    public Property createProperty(@RequestBody Property property) {
+        return propertyRepository.save(property);
+    }
+    //update thong tin bat dong san
+    @PutMapping("/{id}")
+    public Property updateProperty(@PathVariable Long id, @RequestBody Property property) {
+        return propertyRepository.save(property);
+    }
+    //delete bat dong san
+    @DeleteMapping("/{id}")
+    public void deleteProperty(@PathVariable Long id) {
+        propertyRepository.deleteById(id);
+    }
 }

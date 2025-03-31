ba9a67aeaa4155724d723bbbb39661c81ef11d24
dtien18 <tiendt4180@ut.edu.vn>
2025-03-31 21:36:49 +0700

    tao repository

---
+++ HomestayReview/src/main/java/com/homestay/review/repository/HomestayRepository.java
@@ -0,0 +1,7 @@
+package com.homestay.review.repository;
+
+import com.homestay.review.model.Homestay;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HomestayRepository extends JpaRepository<Homestay, Long> {
+}
---
+++ HomestayReview/src/main/java/com/homestay/review/repository/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.homestay.review.repository;
+
+
+import com.homestay.review.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}

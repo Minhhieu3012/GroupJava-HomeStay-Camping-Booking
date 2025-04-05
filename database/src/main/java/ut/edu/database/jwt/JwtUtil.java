5cabd410dabd2ee2971f979bf3d0f3ddd07e93e3
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-04-06 01:18:15 +0700

    create AdminController & update JwtUtil 6/4

--- database/src/main/java/ut/edu/database/jwt/JwtUtil.java
+++ database/src/main/java/ut/edu/database/jwt/JwtUtil.java
@@ -44,6 +44,7 @@ package ut.edu.database.jwt;
 
 import java.util.Date;
 import java.util.Base64;
+
 import jakarta.annotation.PostConstruct;
 import javax.crypto.SecretKey;
 
@@ -52,6 +53,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 @Component
@@ -62,6 +64,7 @@ public class JwtUtil {
 
     private SecretKey secretKey;
 
+    // Khởi tạo secretKey sau khi Spring inject xong
     // Chạy sau khi object được khởi tạo -> decode secretKey từ base64
     @PostConstruct
     public void init() {
@@ -69,6 +72,7 @@ public class JwtUtil {
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
     }
 
+    // Tạo JWT token với username
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
@@ -78,6 +82,7 @@ public class JwtUtil {
                 .compact();
     }
 
+    // Trích xuất username từ token
     public String extractUsername(String token) {
         Claims claims = Jwts.parser()
                 .setSigningKey(secretKey)
@@ -88,8 +93,34 @@ public class JwtUtil {
         return claims.getSubject();
     }
 
+    // Kiểm tra token hợp lệ
     public boolean validateToken(String token, UserDetails userDetails) {
         return extractUsername(token).equals(userDetails.getUsername());
+    }
+
+
+    public String extractRole(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role", String.class);
+    }
+
+    // Kiểm tra token đã hết hạn chưa
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    // Trích xuất toàn bộ Claims
+    private Claims extractClaims(String token) {
+        Jws<Claims> jwsClaims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token);
+
+        return jwsClaims.getBody();
     }
 }
 

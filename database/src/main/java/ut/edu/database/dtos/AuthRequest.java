8cb2c12cf064382566bf7e0d34e00e4317843e73
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-04-06 18:10:02 +0700

    update AuthRequest 6/4

---
+++ database/src/main/java/ut/edu/database/dtos/AuthRequest.java
@@ -0,0 +1,20 @@
+package ut.edu.database.dtos;
+
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class AuthRequest {
+//
+//    private String username;
+//    private String password;
+//    // public String  getUsername(){
+//    //     return this.username;
+//    // }
+//    // public String  getPassword(){
+//    //     return this.password;
+//    // }
+//
+//}

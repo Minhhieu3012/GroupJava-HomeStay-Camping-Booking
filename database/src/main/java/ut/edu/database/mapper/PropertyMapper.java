e587e8efae700b1444244b2f45c9d059e8f380e8
Phan Minh Hieu <hieupm9898@ut.edu.vn>
2025-04-08 07:48:01 +0700

    feat: update Mapper(property,report,review) 8/4

--- database/src/main/java/ut/edu/database/mapper/PropertyMapper.java
+++ database/src/main/java/ut/edu/database/mapper/PropertyMapper.java
@@ -1,4 +1,20 @@
 package ut.edu.database.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import ut.edu.database.models.Property;
+import ut.edu.database.dtos.PropertyDTO;
+
+@Mapper(componentModel = "spring")
 public interface PropertyMapper {
+
+    @Mapping(source = "owner.id", target = "owner_id")
+    PropertyDTO toDTO(Property property);
+
+    @Mapping(target = "owner", ignore = true)
+    @Mapping(target = "bookings", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
+    @Mapping(target = "reports", ignore = true)
+    Property toEntity(PropertyDTO dto);
 }
--- database/src/main/java/ut/edu/database/mapper/ReviewMapper.java
+++ database/src/main/java/ut/edu/database/mapper/ReviewMapper.java
@@ -1,4 +1,18 @@
 package ut.edu.database.mapper;
 
+import ut.edu.database.models.Review;
+import ut.edu.database.dtos.ReviewDTO;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface ReviewMapper {
+    @Mapping(source = "property.id",target = "propertyID")
+    @Mapping(source = "user.id", target = "userID")
+    ReviewDTO toDTO(Review review);
+
+    @Mapping(target = "property", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    Review toEntity(ReviewDTO dto);
 }

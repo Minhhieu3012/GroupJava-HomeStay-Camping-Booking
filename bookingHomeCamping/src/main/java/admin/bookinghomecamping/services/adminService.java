1fb96dc8b9b75d4454601bbddc8667383e122503
baobanpho <baobanpho@gmail.com>
2025-04-08 13:18:44 +0700

    fix bug

--- bookingHomeCamping/src/main/java/admin/bookinghomecamping/services/adminService.java
+++ bookingHomeCamping/src/main/java/admin/bookinghomecamping/services/adminService.java
@@ -1,33 +1,33 @@
-//package admin.bookinghomecamping.services;
-//import admin.bookinghomecamping.models.admin;
-//import admin.bookinghomecamping.repositories.adminRepository;
-//import org.springframework.beans.factory.annotation.Autowire;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import java.util.Optional;
-//import java.util.List;
-//@Service
-//public class adminService {
-//    @Autowired
-//    private adminRepository adminRepository;
-//    public Optional<admin> findByadminName(String adminName) {
-//        return adminRepository.findByAdminName(adminName);
-//    }
-//    public List<admin> getAllAdmins() {
-//        return adminRepository.findAll();
-//    }
-//
-//    //Create admin
-//    public long CreateAdmin(admin admin) {
-//        return adminRepository.save(admin).getId();
-//    }
-//    // Save hoặc update Admin
-//   public admin updateAdmin(admin admin) {
-//        return adminRepository.save(admin);
-//   }
-//
-//    // Xóa Admin theo ID
-//    public void deleteAdmin(Long id) {
-//        adminRepository.deleteById(id);
-//    }
-//}
+package admin.bookinghomecamping.services;
+import admin.bookinghomecamping.models.admin;
+import admin.bookinghomecamping.repositories.adminRepository;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import java.util.List;
+@Service
+public class adminService {
+    @Autowired
+    private adminRepository adminRepository;
+    public Optional<admin> findByadminName(String adminName) {
+        return adminRepository.findByAdminName(adminName);
+    }
+    public List<admin> getAllAdmins() {
+        return adminRepository.findAll();
+    }
+
+    //Create admin
+    public long CreateAdmin(admin admin) {
+        return adminRepository.save(admin).getId();
+    }
+    // Save hoặc update Admin
+   public admin updateAdmin(admin admin) {
+        return adminRepository.save(admin);
+   }
+
+    // Xóa Admin theo ID
+    public void deleteAdmin(Long id) {
+        adminRepository.deleteById(id);
+    }
+}

e1b71370d5de7d3af18f5b6091b145a980f4d3e8
DuckIsStudy <lengohuuducb2.c3hn2020@gmail.com>
2025-04-14 22:44:49 +0700

    done rating

--- HomestayReview/src/main/java/com/homestay/review/service/RatingService.java
+++ HomestayReview/src/main/java/com/homestay/review/service/RatingService.java
@@ -1,23 +1,23 @@
-package com.homestay.review.service;
-
-import com.homestay.review.model.Rating;
-import com.homestay.review.repository.RatingRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class RatingService {
-
-    @Autowired
-    private RatingRepository ratingRepository;
-
-    public Rating saveRating(Rating rating) {
-        return ratingRepository.save(rating);
-    }
-
-    public List<Rating> getRatingsForHomestay(Long homestayId) {
-        return ratingRepository.findAll();  // Add filtering by homestayId later
-    }
-}
+//package com.homestay.review.service;
+//
+//import com.homestay.review.model.Rating;
+//import com.homestay.review.repository.RatingRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class RatingService {
+//
+//    @Autowired
+//    private RatingRepository ratingRepository;
+//
+//    public Rating saveRating(Rating rating) {
+//        return ratingRepository.save(rating);
+//    }
+//
+//    public List<Rating> getRatingsForHomestay(Long homestayId) {
+//        return ratingRepository.findAll();  // Add filtering by homestayId later
+//    }
+//}

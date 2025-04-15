89a49a6f62a2406774a613ae6282ef80c7985657
DuckIsStudy <lengohuuducb2.c3hn2020@gmail.com>
2025-04-15 13:42:13 +0700

    update property

---
+++ HomestayReview/src/main/java/com/homestay/review/controller/testController.java
@@ -0,0 +1,13 @@
+package com.homestay.review.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+    @RequestMapping("/rating")
+    public String test() {
+        return "/Rating/testimonial";
+    }
+
+}

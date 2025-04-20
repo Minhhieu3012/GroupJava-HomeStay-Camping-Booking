42e5855b6a22f33c461817218ea6b083f0aa2a0c
GiaBaoo <ngbao840@gmail.com>
2025-04-21 03:38:06 +0700

    done QuanLiTKChuHomestay-NguoiDung

--- bookingHomeCamping/src/main/java/admin/bookinghomecamping/controller/AdminPageController.java
+++ bookingHomeCamping/src/main/java/admin/bookinghomecamping/controller/AdminPageController.java
@@ -1,13 +1,17 @@
 package admin.bookinghomecamping.controller;
 
 import admin.bookinghomecamping.dto.TransactionDTO;
+import admin.bookinghomecamping.dto.UserDTO;
 import org.springframework.ui.Model;
 //import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")

a10b9ad854dd22a7e4559131f49a2e8cde744fd1
GiaBaoo <ngbao840@gmail.com>
2025-04-23 22:34:05 +0700

    update admin pages, edit menu

--- database/src/main/java/ut/edu/database/controllers/HomeController.java
+++ database/src/main/java/ut/edu/database/controllers/HomeController.java
@@ -34,17 +34,31 @@ public class HomeController {
     public String quanliphidichvuPage() {
         return "bookingHomeCamping-admin/QuanLiPhiDichVu";//goi den html page
     }
-    @GetMapping("/quan-li-tk-host-user")
-    public String quanlitkchuhomestay_nguoidungPage() {
-        return "bookingHomeCamping-admin/QuanLiTKChuHomestay-NguoiDung";//goi den html page
+    @GetMapping("/quan-li-tk-host")
+    public String quanlitkchuhomestayPage() {
+        return "bookingHomeCamping-admin/QuanLiTKChuHomestay";//goi den html page
+    }
+    @GetMapping("/quan-li-tk-user")
+    public String quanlitknguoidungPage() {
+        return "bookingHomeCamping-admin/QuanLiTKNguoiDung";//goi den html page
     }
     @GetMapping("/them-phong")
     public String themphongPage() {
         return "bookingHomeCamping-admin/ThemPhong";//goi den html page
     }
-    @GetMapping("/xoa-phong")
-    public String xoaphongPage() {
-        return "bookingHomeCamping-admin/XoaPhong";//goi den html page
+    @GetMapping("/xem-phong")
+    public String xemphongPage() {
+        return "bookingHomeCamping-admin/XemPhong";//goi den html page
+    }
+
+    @GetMapping("/hoa-don")
+    public String hoadonPage() {
+        return "bookingHomeCamping-admin/HoaDon";//goi den html page
+    }
+
+    @GetMapping("/don-dat-phong")
+    public String dondatphongPage() {
+        return "bookingHomeCamping-admin/DonDatPhong";//goi den html page
     }
 
 }

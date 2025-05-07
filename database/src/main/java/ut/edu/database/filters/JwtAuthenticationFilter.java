6b90e42563cca3a597b259e453a63feed595f8d2
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-05-07 18:07:13 +0700

    last update

--- database/src/main/java/ut/edu/database/filters/JwtAuthenticationFilter.java
+++ database/src/main/java/ut/edu/database/filters/JwtAuthenticationFilter.java
@@ -69,7 +69,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         );
 
                         UsernamePasswordAuthenticationToken authentication =
-                                new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+                                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
                         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                         SecurityContextHolder.getContext().setAuthentication(authentication);
